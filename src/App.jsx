3cde97293855a65357f819f2e92624ad8d48ea58
NabilaFerdousPrapty <128033656+NabilaFerdousPrapty@users.noreply.github.com>
2024-03-20 17:44:30 +0600

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,46 @@
-
-import { useState } from 'react'
-import './App.css'
-import Card from './components/Card/Card'
+import { useCallback, useEffect, useRef, useState } from "react";
+import "./App.css";
+import Card from "./components/Card/Card";
 
 function App() {
- const [length,setLength]=useState(8);
- const [numberAllowed,setNumberAllowed]=useState(false);
- const [charecterAllowed,setCharecterAllowed]=useState(false);
- const [password,setPassword]=useState('')
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [characterAllowed, setCharacterAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+  let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwsyz";
+  if (numberAllowed) str = str + "0123456789";
+  if (characterAllowed) str = str + "!@#$%^&*-_=[]{}~`'";
+  for (let index = 0; index <= length; index++) {
+    let char = Math.floor(Math.random() * str.length + 1);
+    pass += str.charAt(char);
+  }
+  setPassword(pass)
+  }, [
+    length,
+    numberAllowed,
+    characterAllowed,
+    setPassword,
+  ]);
+  useEffect(()=>{
+    passwordGenerator();
+  },[length,numberAllowed,characterAllowed,passwordGenerator])
+  //ref hook
+  const passwordRef=useRef(null)
+  const copyPasswordToClipboard=useCallback(()=>{
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0,101)
+    window.navigator.clipboard.writeText(password)
+  },[password])
   return (
     <>
-   
-      <h1 className='text-4xl text-center p-8 font-bold'>PassWord Generator</h1>
-      <Card length={length} password={password}></Card>
-      
+      <div className="">
+      <h1 className="text-4xl text-center p-8 font-bold">Password Generator</h1>
+      <Card length={length} setLength={setLength} numberAllowed={numberAllowed} characterAllowed={characterAllowed} password={password} setNumberAllowed={setNumberAllowed} setCharacterAllowed={setCharacterAllowed} passwordRef={passwordRef} copyPasswordToClipboard={copyPasswordToClipboard} ></Card>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,28 +1,93 @@
+import PropTypes from "prop-types";
 
+const Card = ({
+  length,
+  password,
+  setLength,
+  numberAllowed,
+  characterAllowed,
+  setNumberAllowed,
+  setCharacterAllowed,
+  passwordRef,
+  copyPasswordToClipboard
+}) => {
+  return (
+    <div>
+      <div className="card lg:card-side bg-gray-800 shadow-xl">
+        <figure>
+          <img
+            className="w-3/4 rounded-2xl"
+            src="https://t3.ftcdn.net/jpg/06/04/74/88/360_F_604748806_gQSyPrazhAocHefqrUtieGBKK22PS5QZ.jpg "
+            alt="Album"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Generate your new password</h2>
+          <div>
+            <input
+              type="text"
+              placeholder="Your Password"
+              className="input input-bordered w-full max-w-xs outline-none"
+              value={password}
+              ref={passwordRef}
+              readOnly
+            />
+            <button className="btn btn-primary hover:bg-slate-900 hover:text-white " onClick={copyPasswordToClipboard}>Copy</button>
+          </div>
 
-const Card = ({length,password}) => {
-    
-    return (
-        <div>
-            <div className="card lg:card-side bg-base-100 shadow-xl">
-  <figure><img className="w-3/4" src="https://t3.ftcdn.net/jpg/06/04/74/88/360_F_604748806_gQSyPrazhAocHefqrUtieGBKK22PS5QZ.jpg " alt="Album"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">Generate your new password</h2>
-    <div><input type="text" placeholder="Your Password" className="input input-bordered w-full max-w-xs" />
-      <button className="btn btn-primary">Copy</button>
-    </div>
-    
-    <div className="card-actions py-8 flex flex-col">
-    <progress className="progress w-56" value={length} max="100"></progress>
-    <div className="flex justify-center items-center gap-4"><input type="checkbox" defaultChecked className="checkbox" />Number</div>
-    <div className="flex justify-center items-center gap-4"><input type="checkbox" defaultChecked className="checkbox" />Character</div>
-      <button className="btn btn-primary">Listen</button>
-    </div>
-  </div>
-</div>
-            
+          <div className="card-actions py-8 flex flex-col">
+            <div className="flex justify-between items-center gap-6">
+              <input
+                type="range"
+                className="w-56"
+                value={length}
+                min={6}
+                max={100}
+                onChange={(e) => setLength(e.target.value)}
+              ></input>
+              <label htmlFor="">length:{length}</label>
+            </div>
+            <div className="flex justify-center items-center gap-4">
+              <input
+                type="checkbox"
+                defaultChecked={numberAllowed}
+                className="checkbox"
+                id="numberInput"
+                onChange={() => {
+                  setNumberAllowed((prev) => !prev);
+                }}
+              />
+              Number
+            </div>
+            <div className="flex justify-center items-center gap-4">
+              <input
+                type="checkbox"
+                defaultChecked={characterAllowed}
+                className="checkbox"
+                id="charInput"
+                onChange={() => {
+                  setCharacterAllowed((prev) => !prev);
+                }}
+              />
+              Character
+            </div>
+            <button className="btn btn-primary">Generate</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
+};
+Card.propTypes = {
+  length: PropTypes.number,
+  password: PropTypes.string,
+  setLength: PropTypes.func,
+  numberAllowed:PropTypes.bool,
+  characterAllowed:PropTypes.bool,
+  setNumberAllowed:PropTypes.func,
+  setCharacterAllowed:PropTypes.func,
+  passwordRef:PropTypes.object.isRequired,
+  copyPasswordToClipboard:PropTypes.func
 };
 
 export default Card;
