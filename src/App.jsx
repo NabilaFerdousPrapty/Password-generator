af0e7132c3b8d52710aeb2f42859837d343f551a
NabilaFerdousPrapty <128033656+NabilaFerdousPrapty@users.noreply.github.com>
2024-03-20 15:27:22 +0600

    design done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 
+import { useState } from 'react'
 import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
- 
-
+ const [length,setLength]=useState(8);
+ const [numberAllowed,setNumberAllowed]=useState(false);
+ const [charecterAllowed,setCharecterAllowed]=useState(false);
+ const [password,setPassword]=useState('')
   return (
     <>
    
       <h1 className='text-4xl text-center p-8 font-bold'>PassWord Generator</h1>
-      <Card></Card>
+      <Card length={length} password={password}></Card>
       
     </>
   )
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,21 @@
 
 
-const Card = () => {
+const Card = ({length,password}) => {
+    
     return (
         <div>
             <div className="card lg:card-side bg-base-100 shadow-xl">
-  <figure><img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
+  <figure><img className="w-3/4" src="https://t3.ftcdn.net/jpg/06/04/74/88/360_F_604748806_gQSyPrazhAocHefqrUtieGBKK22PS5QZ.jpg " alt="Album"/></figure>
   <div className="card-body">
-    <h2 className="card-title">New album is released!</h2>
-    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+    <h2 className="card-title">Generate your new password</h2>
+    <div><input type="text" placeholder="Your Password" className="input input-bordered w-full max-w-xs" />
+      <button className="btn btn-primary">Copy</button>
+    </div>
     
-    
-    <div className="card-actions py-8">
-    <progress className="progress w-56"></progress>
+    <div className="card-actions py-8 flex flex-col">
+    <progress className="progress w-56" value={length} max="100"></progress>
+    <div className="flex justify-center items-center gap-4"><input type="checkbox" defaultChecked className="checkbox" />Number</div>
+    <div className="flex justify-center items-center gap-4"><input type="checkbox" defaultChecked className="checkbox" />Character</div>
       <button className="btn btn-primary">Listen</button>
     </div>
   </div>
